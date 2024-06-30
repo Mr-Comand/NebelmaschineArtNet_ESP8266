bcc42236c3184f842d649d779447ffbc3cc224c5
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-06-30 22:39:28 +0200

    updated to Artnet lib version 0.6.0

--- main/config.h
+++
@@ -1,8 +0,0 @@
-#define relayPin 14
-#define resetPin 2
-#define statusLED LED_BUILTIN
-#define artnetTimeout 10000
-#define otaPassword ""
-
-#define conditional false 
-#define fire_conditional false //Add the condition when it should be able to fire
---
+++ main/fog_config.h
@@ -0,0 +1,7 @@
+#define relayPin 14
+#define resetPin 2
+#define statusLED LED_BUILTIN
+#define artnetTimeout 10000
+#define otaPassword "Technik.00"
+#define fire_conditional false
+#define fire_condition false //Add the condition when it should be able to fire
